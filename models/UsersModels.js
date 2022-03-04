fc536989f660b6108bd4664cf6187e23fb5ca15a
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-03-04 15:37:34 +0100

    fixat tredjepart login

--- models/UsersModels.js
+++ models/UsersModels.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
   username: { type: String, required: true },
   hashedPassword: { type: String, required: true },
 });
